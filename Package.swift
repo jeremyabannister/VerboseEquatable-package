21362313082888cca6ef8315157b6141c817cc61
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2024-11-09 18:01:30 +0100

    feat: update to Swift 6.0

--- Package.swift
+++ Package.swift
@@ -1,37 +1,26 @@
-// swift-tools-version: 5.10
+// swift-tools-version:6.0
 
-///
 import PackageDescription
 
-
-///
 let package = Package(
     name: "VerboseEquatable-package",
     products: [
-        
-        ///
         .library(
             name: "VerboseEquatable-module",
             targets: ["VerboseEquatable-module"]
         ),
     ],
     dependencies: [
-        
-        ///
         .package(
             url: "https://github.com/jeremyabannister/subscript-collection-safely",
-            "0.1.1" ..< "0.2.0"
+            .upToNextMinor(from: "0.1.4")
         ),
-        
-        ///
         .package(
             url: "https://github.com/jeremyabannister/ValueType-package",
-            "0.1.0" ..< "0.2.0"
+            .upToNextMinor(from: "0.1.6")
         ),
     ],
     targets: [
-        
-        ///
         .target(
             name: "VerboseEquatable-module",
             dependencies: [
@@ -39,8 +28,6 @@ let package = Package(
                 .product(name: "ValueType-module", package: "ValueType-package")
             ]
         ),
-        
-        ///
         .testTarget(
             name: "VerboseEquatable-module-tests",
             dependencies: [
